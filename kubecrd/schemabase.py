9354ddcd303e7c4f8e3a537abc4a858a57f24715
Abhilash Raj <raj.abhilash1@gmail.com>
2022-03-19 22:13:36 -0700

    Document things better.

--- kubecrd/schemabase.py
+++ kubecrd/schemabase.py
@@ -5,6 +5,11 @@ from apischema.json_schema import deserialization_schema
 from kubernetes import utils
 from kubernetes.client.models.v1_object_meta import V1ObjectMeta
 
+
+# ObjectMeta_attribute_map is simply the reverse of the : :
+# V1ObjectMeta.attribute_map, which is a mapping from python attribute to json
+# key while this is the opposite from json key to python attribute so that we
+# can pass in the values to instantiate the V1ObjectMeta object.
 ObjectMeta_attribute_map = {
     value: key for key, value in V1ObjectMeta.attribute_map.items()
 }
@@ -18,30 +23,55 @@ class OpenAPISchemaBase:
 
     @classmethod
     def apischema(cls):
-        """"""
+        """Get serialized openapi 3.0 schema for the cls.
+
+        The output is a dict with (possibly nested) key-value pairs based on
+        the schema of the class. This is used to generate the CRD schema down
+        the line which rely on (a subset?) of OpenAPIV3 schema for the
+        definition of a Kubernetes Custom Resource.
+        """
         return deserialization_schema(
             cls, all_refs=False, additional_properties=True, with_schema=False
         )
 
     @classmethod
     def apischema_json(cls):
+        """JSON Serialized OpenAPIV3 schema for the cls."""
         return json.dumps(cls.apischema())
 
     @classmethod
     def apischema_yaml(cls):
+        """YAML Serialized OpenAPIV3 schema for the cls."""
         yaml_schema = yaml.load(cls.apischema_json(), Loader=yaml.Loader)
         return yaml.dump(yaml_schema, Dumper=yaml.Dumper)
 
     @classmethod
     def singular(cls):
+        """Return the 'singular' name of the CRD.
+
+        This is currently just the lower case name of the Python class.
+        """
         return cls.__name__.lower()
 
     @classmethod
     def plural(cls):
+        """Plural name of the CRD.
+
+        This defaults ot just the lower case name of the Python class with an
+        additional 's' in the end of the name. This might not be correct for
+        all CRs though.
+
+        TODO: Make singular and plural a configurable parameter using dunder
+        attributes on cls like ``__group__`` and ``__version__``.
+        """
         return f'{cls.singular()}s'
 
     @classmethod
     def crd_schema_dict(cls):
+        """Return cls serialized as a Kubernetes CRD schema dict.
+
+        This returns a dict representation of the Kubernetes CRD Object of cls.
+        """
         crd = {
             'apiVersion': 'apiextensions.k8s.io/v1',
             'kind': 'CustomResourceDefinition',
@@ -79,6 +109,11 @@ class OpenAPISchemaBase:
 
     @classmethod
     def crd_schema(cls):
+        """Serialized YAML representation of Kubernetes CRD definition for cls.
+
+        This serializes the dict representation from
+        :py:method:`crd_schema_dict` to YAML.
+        """
         return yaml.dump(
             yaml.load(json.dumps(cls.crd_schema_dict()), Loader=yaml.Loader),
             Dumper=yaml.Dumper,
@@ -86,7 +121,13 @@ class OpenAPISchemaBase:
 
     @classmethod
     def from_json(cls, json_data):
-        """Instantiate the class from json value fetched from Kubernetes."""
+        """Instantiate the class from json value fetched from Kubernetes.
+
+        :param json_data: The CR JSON returned from Kubernetes API.
+        :type json_data: Dict
+        :returns: Instantiated cls with the data from json_data.
+        :rtype: cls
+        """
         assert (
             json_data.get('apiVersion') == f'{cls.__group__}/{cls.__version__}'
         )
@@ -101,7 +142,14 @@ class OpenAPISchemaBase:
 
     @classmethod
     def install(cls, k8s_client, exist_ok=True):
-        """Install the CRD in Kubernetes."""
+        """Install the CRD in Kubernetes.
+
+        :param k8s_client: Instantiated Kubernetes API Client.
+        :type k8s_client: kubernetes.client.api_client.ApiClient
+        :param exist_ok: Boolean representing if error should be raised when
+            trying to install a CRD that was already installed.
+        :type exist_ok: bool
+        """
         try:
             utils.create_from_yaml(
                 k8s_client,
